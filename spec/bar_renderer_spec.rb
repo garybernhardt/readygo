94ce44aab74cdcd7249805e04250b7508f710b49
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-04-03 19:02:48 -0700

    add BarRenderer spec

---
+++ spec/bar_renderer_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+class Ready
+  describe BarRenderer do
+    it "renders only the median when there's room for nothing else" do
+      stats = SeriesStatistics.new(1.0, 1.0, 1.0, 1.0, 1.0)
+      renderer = BarRenderer.new(stats, 2.0, 10)
+      renderer.render.should == "|   *    |"
+    end
+
+    it "renders lines connecting the minimum and maximum to the median" do
+      stats = SeriesStatistics.new(1.0, 1.0, 2.0, 3.0, 3.0)
+      renderer = BarRenderer.new(stats, 4.0, 10)
+      renderer.render.should == "|  -*--  |"
+    end
+  end
+end
