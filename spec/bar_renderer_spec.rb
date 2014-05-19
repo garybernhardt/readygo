04eb93d6a0860d286154631c9f07c2c6249d406c
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-05-19 13:34:15 -0700

    update specs for median marker change

--- spec/bar_renderer_spec.rb
+++ spec/bar_renderer_spec.rb
@@ -5,13 +5,13 @@ module Ready
     it "renders only the median when there's room for nothing else" do
       stats = SeriesStatistics.new(1.0, 1.0, 1.0, 1.0, 1.0)
       renderer = BarRenderer.new(stats, 2.0, 10)
-      renderer.render.should == "|   *    |"
+      renderer.render.should == "|   X    |"
     end
 
     it "renders lines connecting the minimum and maximum to the median" do
       stats = SeriesStatistics.new(1.0, 1.0, 2.0, 3.0, 3.0)
       renderer = BarRenderer.new(stats, 4.0, 10)
-      renderer.render.should == "| --*--  |"
+      renderer.render.should == "| --X--  |"
     end
   end
 end
