5ea50906c00134e5a7581fb1322a6c8e5428ba31
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-05-21 01:31:15 -0700

    update bar renderer specs for code changes

--- spec/bar_renderer_spec.rb
+++ spec/bar_renderer_spec.rb
@@ -3,25 +3,25 @@ require "spec_helper"
 module Ready
   describe BarRenderer do
     it "renders only the median when there's room for nothing else" do
-      stats = SeriesStatistics.new(1.0, 1.0, 1.0)
+      stats = SeriesStatistics.new(1.0, 1.0)
       renderer = BarRenderer.new(stats, 2.0, 10)
       renderer.render.should == "|    X   |"
     end
 
     it "renders lines connecting the minimum and maximum to the median" do
-      stats = SeriesStatistics.new(1.0, 2.0, 3.0)
+      stats = SeriesStatistics.new(1.0, 3.0)
       renderer = BarRenderer.new(stats, 4.0, 10)
-      renderer.render.should == "|  --X-- |"
+      renderer.render.should == "|  X---  |"
     end
 
     it "renders points at zero" do
-      stats = SeriesStatistics.new(0.0, 0.0, 0.0)
+      stats = SeriesStatistics.new(0.0, 0.0)
       renderer = BarRenderer.new(stats, 10.0, 10)
       renderer.render.should == "|X       |"
     end
 
     it "renders points at the maximum" do
-      stats = SeriesStatistics.new(10.0, 10.0, 10.0)
+      stats = SeriesStatistics.new(10.0, 10.0)
       renderer = BarRenderer.new(stats, 10.0, 10)
       renderer.render.should == "|       X|"
     end
