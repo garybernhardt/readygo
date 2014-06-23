86ac783576965cfabe888ed91ce58394c4e0d92d
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-06-23 15:40:33 -0700

    move BenchmarkDefinition to its own file

--- lib/ready/values.rb
+++ lib/ready/values.rb
@@ -1,39 +1,4 @@
 module Ready
-  class BenchmarkDefinition < Struct.new(:name,
-                                         :procs,
-                                         :type,
-                                         :repetitions)
-
-    TYPES = [:runtime, :runtime_without_gc, :gc_time]
-
-    extend Forwardable
-    delegate :before_proc => :procs
-    delegate :after_proc => :procs
-    delegate :benchmark_proc => :procs
-    delegate :nothing_proc => :procs
-
-    def initialize(name, procs, type, repetitions=1)
-      raise "BUG: unknown benchmark type #{type.inspect}" unless TYPES.include?(type)
-      super(name, procs, type, repetitions)
-    end
-
-    def enable_gc?
-      type != :runtime_without_gc
-    end
-
-    def record_runtime?
-      [:runtime, :runtime_without_gc].include?(type)
-    end
-
-    def record_gc_time?
-      type == :gc_time
-    end
-
-    def with_repetitions(repetitions)
-      BenchmarkDefinition.new(name, procs, type, repetitions)
-    end
-  end
-
   # The `nothing` attribute exists to provide a way to measure baseline
   # performance to null out benchmarking overhead.
   class BenchmarkProcs < Struct.new(:before_proc,
---
+++ spec/benchmark_definition_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+module Ready
+  describe BenchmarkDefinition do
+    it "raises an exception when the benchmark type is invalid" do
+      expect do
+        BenchmarkDefinition.new("a benchmark", nil, :invalid_benchmark_type)
+      end.to raise_error("BUG: unknown benchmark type :invalid_benchmark_type")
+    end
+  end
+end
